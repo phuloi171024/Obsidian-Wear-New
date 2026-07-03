5f67a51ba897ed5ec300f32c7b7477c50ab23fcc
nhatphi555 <phinnps37704@gmail.com>
2026-07-03 11:38:16 +0700

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,22 +1,19 @@
-import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-
-import Register from './pages/Register'; // Đường dẫn đến file đăng ký của em
-
-// Import 2 trang lớn từ thư mục pages của em
-import Home from './pages/Home';
-import Login from './pages/Login';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import ProductPage from "./pages/ProductPage";
+import CartPage from "./pages/CartPage";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/register" element={<Register />} />
-        {/* Đường dẫn mặc định khi mở web -> Chạy trang chủ Home */}
+
         <Route path="/" element={<Home />} />
+
+        <Route path="/products" element={<ProductPage />} />
+
+        <Route path="/cart" element={<CartPage />} />  
         
-        {/* Đường dẫn khi gõ /login -> Chạy trang đăng nhập Login */}
-        <Route path="/login" element={<Login />} />
       </Routes>
     </BrowserRouter>
   );
