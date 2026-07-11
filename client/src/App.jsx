c4781b6fa51ef13008e990ced153430a927e1868
nhatphi555 <phinnps37704@gmail.com>
2026-07-11 10:24:02 +0700

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,10 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import ProductPage from "./pages/ProductPage";
 import CartPage from "./pages/CartPage";
+import ProductDetail from "./pages/ProductDetail";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import ForgotPassword from "./pages/ForgotPassword";
 
 function App() {
   return (
@@ -13,6 +17,14 @@ function App() {
         <Route path="/products" element={<ProductPage />} />
 
         <Route path="/cart" element={<CartPage />} />  
+
+        <Route path="/product/:id" element={<ProductDetail />} />
+
+        <Route path="/login" element={<Login />} />
+
+<Route path="/register" element={<Register />} />
+
+<Route path="/forgot-password" element={<ForgotPassword />} />
         
       </Routes>
     </BrowserRouter>
