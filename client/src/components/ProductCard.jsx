7a0ae02bb6ab5017a0c38e0e55fd5a8e3654cc18
nhatphi555 <phinnps37704@gmail.com>
2026-07-11 10:18:43 +0700

    Update ProductCard.jsx

--- client/src/components/ProductCard.jsx
+++ client/src/components/ProductCard.jsx
@@ -1,25 +1,29 @@
 import "./ProductCard.css";
+import { Link } from "react-router-dom";
 
 export default function ProductCard({ product }) {
   return (
-    <div className="product-card">
+    <Link
+      to={`/product/${product.id}`}
+      style={{ textDecoration: "none", color: "inherit" }}
+    >
+      <div className="product-card">
+        <img src={product.image} alt={product.name} />
 
-      <img src={product.image} alt={product.name} />
+        <h3>{product.name}</h3>
 
-      <h3>{product.name}</h3>
+        <div className="rating">
+          ★★★★★ <span>({product.rating})</span>
+        </div>
 
-      <div className="rating">
-        ★★★★★ <span>({product.rating})</span>
+        <div className="price-cart">
+          <span className="price">{product.price}</span>
+
+          <button className="cart-btn">
+            <i className="fa-solid fa-cart-shopping"></i>
+          </button>
+        </div>
       </div>
-
-      <div className="price-cart">
-    <span className="price">{product.price}</span>
-
-    <button className="cart-btn">
-        <i className="fa-solid fa-cart-shopping"></i>
-    </button>
-</div>
-
-    </div>
+    </Link>
   );
 }
