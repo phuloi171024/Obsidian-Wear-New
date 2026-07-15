f1ff8886b0d24238e373c057fc6f1807b43bba63
nhatphi555 <phinnps37704@gmail.com>
2026-07-15 09:45:38 +0700

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,34 +1,36 @@
-import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Register from './pages/Register'; 
-import Home from "./pages/Home"; 
-import ProductPage from "./pages/ProductPage"; 
-import Login from './pages/Login';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import ProductPage from "./pages/ProductPage";
 import CartPage from "./pages/CartPage";
-import PaymentPage from "./pages/PaymentPage";
 import ProductDetail from "./pages/ProductDetail";
-import Contact from "./pages/Contact/Contact";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 import ForgotPassword from "./pages/ForgotPassword";
+import Contact from "./pages/Contact/Contact";
+import CheckoutPage from "./pages/CheckoutPage";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/products" element={<ProductPage />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/cart" element={<CartPage />} />
-        <Route path="/payment" element={<PaymentPage />} />
-        <Route path="/product/:id" element={<ProductDetail />} />
-        <Route path="/contact" element={<Contact />} />
 
-        {/* Đường dẫn khi gõ /login -> Chạy trang đăng nhập Login */}
+        <Route path="/" element={<Home />} />
+
+        <Route path="/products" element={<ProductPage />} />
+
+        <Route path="/cart" element={<CartPage />} />  
+
+        <Route path="/product/:id" element={<ProductDetail />} />
+
         <Route path="/login" element={<Login />} />
 
 <Route path="/register" element={<Register />} />
 
 <Route path="/forgot-password" element={<ForgotPassword />} />
+
+<Route path="/contact" element={<Contact />} />
         
+        <Route path="/checkout" element={<CheckoutPage />} />
       </Routes>
     </BrowserRouter>
   );
