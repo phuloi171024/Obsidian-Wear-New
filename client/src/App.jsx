c6452acb11ad5beff04d27e5619bdc2d1b2ea491
nhatphi555 <phinnps37704@gmail.com>
2026-07-25 00:34:33 +0700

    update app.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,40 +1,54 @@
-import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Header from './components/Header';
-import Register from './pages/Register'; 
-import Home from "./pages/Home"; 
-import ProductPage from "./pages/ProductPage"; 
-import ProductDetail from "./pages/ProductDetail";
-import Login from './pages/Login';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import ProductPage from "./pages/ProductPage";
 import CartPage from "./pages/CartPage";
-import PaymentPage from "./pages/PaymentPage";
-import ForgotPassword from './pages/ForgotPassword';
+import ProductDetail from "./pages/ProductDetail";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import ForgotPassword from "./pages/ForgotPassword";
 import Contact from "./pages/Contact/Contact";
 import CheckoutPage from "./pages/CheckoutPage";
 import OrdersPage from "./pages/OrdersPage";
 import ProfilePage from "./pages/ProfilePage";
 import SizeGuide from "./pages/SizeGuide";
+import AdminLayout from "./pages/Admin/AdminLayout";
+import Dashboard from "./pages/Admin/Dashboard";
+import Orders from "./pages/Admin/Orders";
+import Products from "./pages/Admin/Products";
+import Categories from "./pages/Admin/Categories";
+import Users from "./pages/Admin/Users";
+import Statistics from "./pages/Admin/Statistics";
+import Comments from "./pages/Admin/Comments";
 
 function App() {
   return (
     <BrowserRouter>
-      <Header /> 
-      
       <Routes>
+
         <Route path="/" element={<Home />} />
         <Route path="/products" element={<ProductPage />} />
-        <Route path="/product/:id" element={<ProductDetail />} />
-        <Route path="/register" element={<Register />} />
         <Route path="/cart" element={<CartPage />} />
-        <Route path="/payment" element={<PaymentPage />} />
+        <Route path="/product/:id" element={<ProductDetail />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
         <Route path="/forgot-password" element={<ForgotPassword />} />
         <Route path="/contact" element={<Contact />} />
         <Route path="/checkout" element={<CheckoutPage />} />
         <Route path="/orders" element={<OrdersPage />} />
         <Route path="/profile" element={<ProfilePage />} />
-
         <Route path="/size-guide" element={<SizeGuide />} />
+
+        {/* ADMIN */}
+        <Route path="/admin" element={<AdminLayout />}>
+          <Route index element={<Dashboard />} />
+          <Route path="orders" element={<Orders />} />
+          <Route path="products" element={<Products />} />
+          <Route path="categories" element={<Categories />} />
+          <Route path="users" element={<Users />} />
+          <Route path="statistics" element={<Statistics />} />
+          <Route path="comments" element={<Comments />} />
+        </Route>
+
       </Routes>
     </BrowserRouter>
   );
