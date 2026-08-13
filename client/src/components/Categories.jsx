f3f485a77d696e495589e0273e571deb5c62caa4
nhatphi555 <phinnps37704@gmail.com>
2026-08-13 14:35:42 +0700

    Update Categories

--- client/src/components/Categories.jsx
+++ client/src/components/Categories.jsx
@@ -1,50 +1,52 @@
 import "./Categories.css";
 
+import aoPhongCotton from "../public/images/aophongcotton.png";
+import quanJeans from "../public/images/quanjeans.png";
+import aoKhoacBomberNu from "../public/images/aokhoacbombernu.png";
+import aoSoMi from "../public/images/aosomi.png";
+
 const categories = [
   {
     title: "Áo Phông Cotton Cơ Bản",
-    image: "/src/public/images/aophongcotton.png",
+    image: aoPhongCotton,
   },
   {
     title: "Quần Jeans Ôm Vừa",
-    image: "/src/public/images/quanjeans.png",
+    image: quanJeans,
   },
   {
     title: "Áo Khoác Bomber Nữ",
-    image: "/src/public/images/aokhoacbombernu.png",
+    image: aoKhoacBomberNu,
   },
   {
     title: "Áo Sơ Mi",
-    image: "/src/public/images/aosomi.png",
+    image: aoSoMi,
   },
 ];
 
 export default function Categories() {
   return (
-    <section className="featured-products categories-section">
+    <section className="categories-section">
 
       <div className="section-header">
-        <div>
-          <h2>Danh mục sản phẩm</h2>
-          <p>Tìm kiếm danh mục phù hợp với bạn</p>
-        </div>
+        <h2>Danh mục sản phẩm</h2>
+        <p>Tìm kiếm danh mục phù hợp với bạn</p>
       </div>
 
       <div className="categories-grid">
         {categories.map((item, index) => (
           <div
-            className="category-card"
+            className="home-category-card"
             key={index}
             style={{
               backgroundImage: `url(${item.image})`,
             }}
           >
             <div className="overlay">
-
               <h3>{item.title}</h3>
-
-              <span>Khám phá ngay →</span>
-
+              <button type="button" className="explore-button">
+                Khám phá ngay →
+              </button>
             </div>
           </div>
         ))}
